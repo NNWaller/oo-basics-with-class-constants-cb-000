daa5c9d4e1e8a9033e71270e96cca95db9f82291
Natasha Waller <nnwaller@gmail.com>
2019-01-03 20:19:22 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,7 +3,7 @@ class Book
   attr_reader :title, :genre
 
   GENRES = [ ]
-  
+
   def initialize(title)
     @title = title
   end
@@ -16,5 +16,4 @@ class Book
     @genre = genre
     GENRES << genre
   end
-
 end
